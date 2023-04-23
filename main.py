3b58fdb60d3a14791044b0677d7b690cd31ef9fe
Ritika <116043234+ritikazahira@users.noreply.github.com>
2023-04-22 20:45:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math, queue
+import math
+import queue
 from collections import Counter
 
 class TreeNode(object):
@@ -34,28 +35,51 @@ def make_huffman_tree(f):
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
+        x = p.get()
+        y = p.get()
+        z = TreeNode(x, y, (x.data[0]+y.data[0], ""))
+        p.put(z)
         
     # return root of the tree
     return p.get()
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    if node.left is None and node.right is None:
+        code[node.data[1]] = prefix
+        return code
+    code = get_code(node.left,prefix + "0", code)
+    code = get_code(node.right, prefix + "1", code)
+    return code
+
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+    cost = 0
+    for c in f.keys():
+        cost += math.ceil(math.log2(f[c]))
+    return cost
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+    cost = 0
+    for c in C.keys():
+        if c in f:
+            cost += f[c] * len(C[c])
+    return cost
+
+files = ['alice29.txt','asyoulik.txt','f1.txt','fields.c','grammar.lsp']
+for file in files:
+    f = get_frequencies(file)
+    print("Fixed-length cost for {}: {}".format(file,fixed_length_cost(f)))
+    T = make_huffman_tree(f)
+    C = get_code(T)
+    print("Huffman cost for {}: {}".format(file,huffman_cost(C, f)))    
 
 f = get_frequencies('f1.txt')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
 T = make_huffman_tree(f)
+C = get_code(T)
+print("Huffman cost:  %d" % huffman_cost(C, f))
 C = get_code(T)
 print("Huffman cost:  %d" % huffman_cost(C, f))
